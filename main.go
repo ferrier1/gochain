bf3e0da3b18b4377e411b899df462062420beea4
johnnydrama92 <james.ferrier01@gmail.com>
2018-11-05 21:14:39 +0000

    websrv func

--- main.go
+++ main.go
@@ -89,3 +89,25 @@ func replaceChain(newBlocks []Block) {
     Blockchain = newBlocks
   }
 }
+
+// this is a webserver function to view the Blockchain
+
+
+func run() error {
+  mux := makeMuxRouter()
+  httpAddr := os.Getenv("ADDR")
+  log.Println("Listening on ", os.Getenv("ADDR"))
+  s := &http.Server{
+    Addr:             ":" + httpAddr,
+    Handler:          mux,
+    ReadTimeout:      10 * time.Second,
+    WriteTimeout:     10 * time.Second,
+    MaxHeaderBytes:   1 << 20,
+  }
+
+  if err := s.ListenAndServe(); err != nil {
+    return err
+  }
+
+  return nil
+}
